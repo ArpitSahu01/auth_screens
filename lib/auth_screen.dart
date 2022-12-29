bc93f37d30b9fbaf97b11b188dd2f6099d30ca36
arpit <arpitsahu789@gmail.com>
2022-12-29 11:53:20 +0530

    Initial commit

---
+++ lib/auth_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+
+  static const routeName = '/auth-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './auth_screen.dart';
+
+void main(){
+
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Auth Screen',
+      home: AuthScreen(),
+    );
+  }
+
+}
